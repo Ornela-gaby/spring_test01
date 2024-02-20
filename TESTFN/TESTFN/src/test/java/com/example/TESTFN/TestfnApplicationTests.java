9b0e3536605e5c25a6d0562ae9193207a682411f
unknown <toukamornela5@gmail.com>
2024-02-20 20:10:01 +0100

    ajout du projet spring boot avec start.io

---
+++ TESTFN/TESTFN/src/test/java/com/example/TESTFN/TestfnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TESTFN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestfnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
